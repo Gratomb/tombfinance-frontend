e14ca0e5d872bfaff21697e31a3fc6aa15041120
Gratomb <98720738+Gratomb@users.noreply.github.com>
2022-02-09 14:19:18 -0500

    dsadsad

--- src/components/TokenSymbol/TokenSymbol.tsx
+++ src/components/TokenSymbol/TokenSymbol.tsx
@@ -11,11 +11,10 @@ import hyehFtmLpLogo from '../../assets/img/hyeh_ftm_lp.png';
 import yehShareFtmLpLogo from '../../assets/img/yehshare_ftm_lp.png';
 
 import wftmLogo from '../../assets/img/ftm_logo_blue.svg';
-import BondLogo from '../../assets/img/spooky.png';
+import bondLogo from '../../assets/img/spooky.png';
 import tombLogo from '../../assets/img/tomb_logo.png';
 import tShareLogo from '../../assets/img/tshare_logo.png';
 import daiLogo from '../../assets/img/dai_logo.png';
-import:
 
 const logosBySymbol: { [title: string]: string } = {
   //Real tokens
